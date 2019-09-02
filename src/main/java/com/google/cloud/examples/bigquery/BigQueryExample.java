24cac436d94108fdbbd040eba6468a0c57afbbc4
hajime <github@hajigle.com>
2019-09-02 16:04:04 +1000

    Compiled with google-cloud-bigquery 1.66.0 (and it created bunch of files)

--- src/main/java/com/google/cloud/examples/bigquery/BigQueryExample.java
+++ src/main/java/com/google/cloud/examples/bigquery/BigQueryExample.java
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *
+ * @see https://cloud.google.com/bigquery/docs/tables
  */
 
 package com.google.cloud.examples.bigquery;
